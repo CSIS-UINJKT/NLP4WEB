343ae2eff845047f8927e7a50550a2e243a2d406
alvinrindra <alvinrindra@gmail.com>
2017-11-21 15:46:27 +0100

    Complete Pipeline4 Skeleton

--- Ex1/de.unihamburg.informatik.nlp4web.tutorial.tut1/src/main/java/de/unihamburg/informatik/nlp4web/tutorial/tut4/starterCode/Pipeline4.java
+++ Ex1/de.unihamburg.informatik.nlp4web.tutorial.tut1/src/main/java/de/unihamburg/informatik/nlp4web/tutorial/tut4/starterCode/Pipeline4.java
@@ -14,6 +14,10 @@ import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordLemmatizer;
 import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordParser;
 import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordPosTagger;
 import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordSegmenter;
+import de.unihamburg.informatik.nlp4web.tutorial.tut4.reader.HearstAnnotator;
+import de.unihamburg.informatik.nlp4web.tutorial.tut4.writer.HearstDebugWriter;
+import de.unihamburg.informatik.nlp4web.tutorial.tut4.writer.HearstWriter;
+import de.unihamburg.informatik.nlp4web.tutorial.tut4.reader.RemoveOtherAnd;
 import de.unihamburg.informatik.nlp4web.tutorial.tut4.writer.ChunkWriter;
 import de.unihamburg.informatik.nlp4web.tutorial.tut4.writer.LemmaWriter;
 import de.unihamburg.informatik.nlp4web.tutorial.tut4.writer.POSWriter;
@@ -32,5 +36,45 @@ public class Pipeline4 {
 						+ "hypognathous and opisthognathous head. In prognathous insects, the vertex is not found between the compound eyes, but rather, where the ocelli are normally."
 						+ " This is because the primary axis of the head is rotated 90° to become parallel to the primary axis of the body. In some species, this region"
 						+ " is modified and assumes a different name.");
+
+		JCas jcas2 = JCasFactory.createJCas();
+
+		jcas2.setDocumentLanguage("en");
+		jcas2.setDocumentText(
+
+				"Dogs such as doge. Such dogs as doge. Houses such as shacks. Big animals such as cats, dogs. "
+						+ "Animals, including cats. " + "Animals, especially cats. " + "Such animals as cats. "
+						+ "Cats, and other animals. " + "Cats or other animals");
+
+		AnalysisEngine writerA = createEngine(HearstWriter.class, HearstWriter.MODE, HearstWriter.MOST_FOUND);
+		AnalysisEngine writerB = createEngine(HearstWriter.class, HearstWriter.MODE, HearstWriter.MOST_PRODUCTIVE);
+		AnalysisEngine seg = createEngine(StanfordSegmenter.class);
+
+		AnalysisEngine lem = createEngine(StanfordLemmatizer.class);
+
+		AnalysisEngine pos = createEngine(StanfordPosTagger.class);
+
+		AnalysisEngine parse = createEngine(StanfordParser.class);
+		AnalysisEngine dechunker = createEngine(RemoveOtherAnd.class);
+		AnalysisEngine hearst = createEngine(HearstAnnotator.class);
+
+		AnalysisEngine hWriter = createEngine(HearstDebugWriter.class);
+		AnalysisEngine posWriter = createEngine(POSWriter.class);
+		AnalysisEngine lemmaWriter = createEngine(LemmaWriter.class);
+		AnalysisEngine chunkWriter = createEngine(ChunkWriter.class);
+
+        SimplePipeline.runPipeline(
+        		jcas2,
+        		seg,
+        		pos,
+        		lem,
+        		parse,
+        		posWriter,
+        		lemmaWriter,
+        		chunkWriter
+        );
+        
+		SimplePipeline.runPipeline(jcas2, seg, pos, lem, parse, dechunker, hearst, writerA, writerB);
+//		SimplePipeline.runPipeline(jcas, seg, pos, lem, parse, dechunker, hearst, writerA, writerB);
 	}
 }
